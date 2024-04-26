4b18f75db552be0f73bc831f4691626fe776a3a4
administrator <ikackac2000@gmail.com>
2024-04-26 14:46:23 +0000

    adding new photos

--- jackal/clearpath_nav2/launch/localization.launch.py
+++ jackal/clearpath_nav2/launch/localization.launch.py
@@ -106,7 +106,7 @@ def generate_launch_description():
 
     map_arg = DeclareLaunchArgument(
         'map',
-        default_value=PathJoinSubstitution([pkg_clearpath_nav2, 'maps', 'site1.yaml']),
+        default_value=PathJoinSubstitution([pkg_clearpath_nav2, 'maps', 'maps.yaml']),
         description='Full path to map yaml file to load')
 #Map
     ld = LaunchDescription(ARGUMENTS)
